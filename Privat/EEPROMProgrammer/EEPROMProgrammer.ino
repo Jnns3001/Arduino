11205654ff6ac35f3d1ebde9243d15962b459924
Jnns3001 <78305903+Jnns3001@users.noreply.github.com>
2022-12-02 19:32:41 +0100

    started ecc

--- Privat/EEPROMProgrammer/EEPROMProgrammer.ino
+++ Privat/EEPROMProgrammer/EEPROMProgrammer.ino
@@ -1,8 +1,3 @@
-/**
- * Code for Programmer
- * 
-*/
-
 #define SHIFT_DATA 2
 #define SHIFT_CLK 4
 #define SHIFT_LATCH 3
@@ -29,7 +24,6 @@ void init(){
   Serial.println("intialising Serial COM");
 }
 
-
 void setIO(bool Input){
   if(Input){
     for (int pin = DATA0; pin >= DATA7; pin -=1){
@@ -43,7 +37,6 @@ void setIO(bool Input){
   }
 }
 
-
 void setAddress(int address) {
   shiftOut(SHIFT_DATA, SHIFT_CLK, LSBFIRST, address);
   shiftOut(SHIFT_DATA, SHIFT_CLK, LSBFIRST, (address >> 8));
@@ -54,14 +47,12 @@ void setAddress(int address) {
   digitalWrite(SHIFT_LATCH, LOW);
 }
 
-
 void setByte(byte out){
   setIO(0);
   for(int i=0; i<8; i++){
     digitalWrite(DATA0 - i, bitRead(out, i));
   }
 }
-
 
 byte readByte(int address){
   setIO(1);
@@ -81,7 +72,6 @@ byte readByte(int address){
   return data;
 }
 
-
 void writeByte(byte data, int address){
   setIO(0);
   digitalWrite(OE, HIGH);
@@ -96,7 +86,6 @@ void writeByte(byte data, int address){
 
 }
 
-
 void printContents(int len) {
   setIO(1);
 
@@ -108,7 +97,7 @@ void printContents(int len) {
     char buf[500];
     sprintf(
       buf,
-      "%03x: %02x %02x %02x %02x %02x %02x %02x %02x   %02x %02x %02x %02x %02x %02x %02x %02x   |    %02x %02x %02x %02x %02x %02x %02x %02x   %02x %02x %02x %02x %02x %02x %02x %02x",
+      "%03x: %02x %02x %02x %02x %02x %02x %02x %02x  %02x %02x %02x %02x %02x %02x %02x %02x  |  %02x %02x %02x %02x %02x %02x %02x %02x  %02x %02x %02x %02x %02x %02x %02x %02x",
       base, 
       data[0],  data[1],  data[2],  data[3],  data[4],  data[5],  data[6],  data[7],  data[8],  data[9],  data[10], data[11], data[12], data[13], data[14], data[15],
       data[16], data[17], data[18], data[19], data[20], data[21], data[22], data[23], data[24], data[25], data[26], data[27], data[28], data[29], data[30], data[31]);
@@ -117,15 +106,13 @@ void printContents(int len) {
   }
 }
 
-
-void programData(byte data, int address){
+void programByte(byte data, int address){
   setIO(0);
 
   writeByte(data, address);
   
   delayMicroseconds(30);
 }
-
 
 void eraseChip(){
   setIO(0);
@@ -135,19 +122,20 @@ void eraseChip(){
   }
 }
 
+void programData(byte[] data, int[] address){
+  
 
+
+}
 
 void setup(){
   init();
 
   eraseChip();
-  programData(0x45, 0x000F);
+  programByte(0x45, 0x000F);
 
   printContents(0x10);
 }
 
-
 void loop() {
 }
-
-
